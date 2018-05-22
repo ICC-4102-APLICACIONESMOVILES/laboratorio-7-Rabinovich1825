cf3a13995d84111dc0a8734d57f24f167026eaf9
Rabinovich1825 <baperez@miuandes.cl>
2018-05-22 12:26:08 -0400

    notification

--- app/src/main/java/com/example/rabinovich/broadcasts/AlarmThing.java
+++ app/src/main/java/com/example/rabinovich/broadcasts/AlarmThing.java
@@ -1,17 +1,32 @@
 package com.example.rabinovich.broadcasts;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
+
+import java.util.Random;
 
 /**
  * Created by Rabinovich on 5/22/2018.
  */
 
 public class AlarmThing extends BroadcastReceiver {
+    private int ID;
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "ALARM!!!", Toast.LENGTH_SHORT).show();
+
+        Random rn = new Random();
+        ID = rn.nextInt();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "channelID")
+                .setSmallIcon(R.drawable.ic_alarm)
+                .setContentTitle("My notification")
+                .setContentText("ALARM!!!!");
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(ID, builder.build());
+
     }
 }
--- app/src/main/java/com/example/rabinovich/broadcasts/MainActivity.java
+++ app/src/main/java/com/example/rabinovich/broadcasts/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rabinovich.broadcasts;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,5 +17,6 @@ public class MainActivity extends AppCompatActivity {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 23423423, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
         alarmManager.setInexactRepeating(AlarmManager.RTC, System.currentTimeMillis(), 5000, pendingIntent);
+
     }
 }
